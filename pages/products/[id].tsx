d7040767ec02ab389222745c5a72831900a30384
tancd <caotan1008@gmail.com>
2022-07-13 07:10:04 +0700

    update

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,11 +1,36 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
 import React from 'react'
 
-type Props = {}
-
-const Productdetail = (props: Props) => {
-  return (
-    <div>Productdetail</div>
-  )
+type ProductProps = {
+  product:any
 }
 
+const Productdetail = ({product}: ProductProps) => {
+  return (
+    <div>Productdetail: {product.name}</div>
+  )
+}
+export const getStaticPaths: GetStaticPaths = async () => {
+  const data = await (await fetch(`http://localhost:3001/products`)).json();
+  const paths = data.map((item: any) => {
+    return { params: { id: item.id} }
+  })
+  return {
+    paths,
+    fallback: "blocking"
+  }
+}
+export const getStaticProps: GetStaticProps<ProductProps> = async (context: GetStaticPropsContext) => {
+  const data = await (await fetch(`http://localhost:3001/products/${context.params?.id}`)).json();
+  if(!data){
+    return {
+      notFound:true
+    }
+  }
+  return {
+    props: {
+      product: data
+    }
+  }
+}
 export default Productdetail
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,24 +1,30 @@
 import { GetStaticProps, GetStaticPropsContext } from 'next';
+import Link from 'next/link';
 import React from 'react'
 
-type ProductProps = {
+type ProductsProps = {
   products: any[];
 }
 
-const Products = ({products}: ProductProps) => {
+const Products = ({products}: ProductsProps) => {
   if(!products) return null;
   return (
     <div>
      {products.map((item)=> (
-      <div key={item.id}>{item.name}</div>
+      <div key={item.id}><Link href={`/products/${item.id}`}>{item.name}</Link></div>
      ))}
     </div>
   )
 }
-
-export const getStaticProps: GetStaticProps<ProductProps> = async (context: GetStaticPropsContext) => {
+// Chạy ở server
+export const getStaticProps: GetStaticProps<ProductsProps> = async (context: GetStaticPropsContext) => {
   console.log("Get static props - server");
   const data = await (await fetch('http://localhost:3001/products')).json();
+  if(!data){
+    return{
+      notFound: true
+    }
+  }
   return {
     props: {
       products: data
